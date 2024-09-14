4289baac8ea390da0721a6ca75e23f847dd6750e
02loveslollipop <mmonsalve2005@gmail.com>
2024-09-14 16:48:09 -0500

    Refactor gRPC compilation script and new gRPC functions implementation

--- gRPC/compile.sh
+++ gRPC/compile.sh
@@ -2,8 +2,3 @@ python -m grpc_tools.protoc -I. --python_out=../Common --pyi_out=../Common --grp
 cp ../Common/p2p_pb2.py         ../Peer/p2p_pb2.py
 cp ../Common/p2p_pb2_grpc.py    ../Peer/p2p_pb2_grpc.py
 cp ../Common/p2p_pb2.pyi        ../Peer/p2p_pb2.pyi
-
-
-cp ../Common/p2p_pb2.py         ../Server/p2p_pb2.py
-cp ../Common/p2p_pb2_grpc.py    ../Server/p2p_pb2_grpc.py
-cp ../Common/p2p_pb2.pyi        ../Server/p2p_pb2.pyi
