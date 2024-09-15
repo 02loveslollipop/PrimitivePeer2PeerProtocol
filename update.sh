44a2d4aa73e81bb4276744b4734c0c847eee1e80
02loveslollipop <mmonsalve2005@gmail.com>
2024-09-15 15:02:56 -0500

    Update setup.sh and update.sh scripts

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
-sudo apt update
-sudo apt install docker-compose -y
-#Build images
+#Stop docker compose
+sudo docker-compose stop
+#rebuild images
+clear
 sudo docker build -t p4_peer:0.0.1 Peer/
 sudo docker build -t p4_server:0.0.1 Server/
-#running docker compose
+#Execute again docker compose
+clear
 sudo docker-compose -f docker-compose.yml up -d
