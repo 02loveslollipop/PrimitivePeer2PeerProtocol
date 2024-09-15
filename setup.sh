7ca98ba99c12b00ec8869d2fefa06b8e3d3a8f39
02loveslollipop <mmonsalve2005@gmail.com>
2024-09-15 18:55:27 -0500

    Update docker-compose.yml to use specific version for Peer services

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install docker-compose -y
 #Build images
-sudo docker build -t p4_peer:0.0.1 Peer/
-sudo docker build -t p4_server:0.0.1 Server/
+sudo docker build -t p4_peer:0.0.1 ./Peer/
+sudo docker build -t p4_server:0.0.1 ./Server/
 #running docker compose
 sudo docker-compose -f docker-compose.yml up -d
